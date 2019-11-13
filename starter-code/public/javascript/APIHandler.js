ab38a30fdd3c404c14dcc1d30d65e961f0bd69f3
camille <camille.grison@gmail.com>
2019-11-13 19:38:21 +0100

    iteration 2 to create

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,27 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.service = axios.create({ //creation of a connextion between client and server with axios
+      baseURL: baseUrl // based on a URL as parameter (line 1...and use in the index.js file line 1)
+    });
   }
 
-  getFullList () {
+  getFullList() {
+    return this.service.get("/characters");
+  }
+
+  getOneRegister(id) {
+    return this.service.get(`/characters/${id}`);
+  }
+
+  createOneRegister(infos) {
+    return this.service.post(`/characters`, infos)
+  }
+
+  updateOneRegister() {
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,13 +1,56 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:8000'); // parameter is defined in APIHandlers line 2
+console.log(charactersAPI);
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  const container = document.querySelector(".characters-container");
 
-  });
+  function characterCards(char) { // paramètre correspondant à la data de notre base de données
+    return `
+    <div class="character-info">
+      <div class="name">Character Name: ${char.name}</div>
+      <div class="occupation">Character Occupation: ${char.occupation}</div>
+      <div class="cartoon">Is a Cartoon?: ${char.cartoon}</div>
+      <div class="weapon">Character Weapon: ${char.weapon}</div>
+    </div>`;
+  }
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  function showAll() {
+    charactersAPI
+      .getFullList()
+      .then(apiRes => {
+        const characters = apiRes.data; // ici characters correspond à une array
+        console.log(characters)
+        let template = "";
+        characters.forEach(char => { // car est la position de notre index
+          template += characterCards(char);
+        });
+        container.innerHTML = template;
+      }).catch(err => console.log(err));
+  }
 
-  });
+
+  document
+    .getElementById('fetch-all')
+    .addEventListener('click', () => {
+      console.log("click");
+      showAll();
+    });
+
+  document
+    .getElementById('fetch-one')
+    .addEventListener('click', function (event) {
+      const id = document.getElementById("characterId").value; // on a eu besoin de définir un #id dans notre input pour avoir accès à sa value directement
+      // console.log(id);
+      charactersAPI
+        .getOneRegister(id)
+        .then(apiRes => {
+          // console.log(apiRes);
+          const character = apiRes.data;
+          container.innerHTML = characterCards(character);
+        }).catch(err => console.log(err));
+    });
+
+
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
@@ -18,6 +61,26 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const newCharacter = {
+      name: document.querySelector(`#new-character-form input[name="name"]`).value,
+      occupation: document.querySelector(`#new-character-form input[name="occupation"]`).value,
+      weapon: document.querySelector(`#new-character-form input[name="weapon"]`).value,
+      cartoon: document.querySelector(`#new-character-form input[name="cartoon"]`).checked,
+    };
+    if (!newCharacter.name || !newCharacter.occupation || !newCharacter.weapon) document.getElementById("send-data").classList.toggle("red")
+    else {
+      charactersAPI
+        .createOneRegister(newCharacter)
+        .then(() => {
+          document.getElementById("send-data").classList.toggle("green");
+          document.getElementById("send-data").classList.remove("red")
+          setTimeout(() => document.getElementById("send-data").classList.toggle("green"), 2000)
+        })
+        .catch(err => {
+          console.log(err);
+          document.getElementById("send-data").classList.toggle("red");
+        });
+    }
   });
 });
